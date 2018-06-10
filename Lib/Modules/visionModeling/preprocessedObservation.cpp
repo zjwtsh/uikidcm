01cc403375faa969c5b63450167f09b859c49145
tanqingge <964463975@qq.com>
2018-06-10 10:18:13 +0800

    first time

--- Lib/Modules/visionModeling/preprocessedObservation.cpp
+++ Lib/Modules/visionModeling/preprocessedObservation.cpp
@@ -48,203 +48,8 @@ void available_segments_init()
 }
 */
 
-//transfer the points from the robotic coordinate to world coordinate:
-void preprocessedObservation::coor_trans(MatrixWrapper::ColumnVector state) const
-{
-	double tran_x = state(1);
-	double tran_y=  state(2);
-	double tran_theta = state(3);
 
-	int valid_segments = (int)available_segments.size();
 
-	std::vector<SegmentStats> &s = available_segments;
-
-	for (int i = 0; i < valid_segments; i++)
-	{
-		double x0_temp=s[i].x0;
-		double y0_temp=s[i].y0;
-		double x1_temp=s[i].x1;
-		double y1_temp=s[i].y1;
-
-		s[i].x0 = cos(tran_theta)*x0_temp - sin(tran_theta)*y0_temp + tran_x;
-	  s[i].y0 = sin(tran_theta)*x0_temp + cos(tran_theta)*y0_temp + tran_y;
-		s[i].x1 = cos(tran_theta)*x1_temp - sin(tran_theta)*y1_temp + tran_x;
-		s[i].y1 = sin(tran_theta)*x1_temp + cos(tran_theta)*y1_temp + tran_y;
-	}
-
-	return;
-}
-
-//carculate the number of points in lines:
-int preprocessedObservation::plot_lines(void) const 
-{
-	int p = 0;
-	int valid_segments = (int)available_segments.size();
-	linepoint.clear();
-
-	for (int num = 0; num < valid_segments; num++)
-	{
-		double kx0 = available_segments[num].x0;
-		double kx1 = available_segments[num].x1;
-		double ky0 = available_segments[num].y0;
-		double ky1 = available_segments[num].y1;
-		//line is vertical
-		if (kx0 == kx1)
-		{
-			int x_temp = round((kx0 - leftboudary) / divided_length) + 1;
-			int y_s, y_e;
-			if (ky0 <= ky1) 
-			{
-				y_s = ceil((ky0 - lowboundary) / divided_length) + 1;
-				y_e = floor((ky1 - lowboundary) / divided_length) + 1;
-			}
-			else
-			{
-				y_s = ceil((ky1 - lowboundary) / divided_length) + 1;
-				y_e = floor((ky0 - lowboundary) / divided_length) + 1;
-			}
-			for (int j = y_s; j <= y_e; j++) 
-			{
-				Line_points TEMPRO1 = { x_temp ,j ,num ,0 };
-				linepoint.push_back(TEMPRO1);
-				p++;
-			}
-		}
-		//line is horizontal
-		else if (ky0 == ky1)
-		{
-			int y_temp = round((ky0 - lowboundary) / divided_length) + 1;
-			int x_s, x_e;
-			if (kx0 <= kx1) 
-			{
-				x_s = ceil((kx0 - leftboudary) / divided_length) + 1;
-				x_e = floor((kx1 - leftboudary) / divided_length) + 1;
-			}
-			else 
-			{
-				x_s = ceil((kx1 - leftboudary) / divided_length) + 1;
-				x_e = floor((kx0 - leftboudary) / divided_length) + 1;
-			}
-			for (int j = x_s; j <= x_e; j++)
-			{
-				Line_points TEMPRO1 = { j ,y_temp ,num ,90 };
-				linepoint.push_back(TEMPRO1);
-				p++;
-			}
-		}
-		else 
-		{
-			double grad = (ky1 - ky0) / (kx1 - kx0);
-			double theta0 = atan(grad) * 180 / pi;
-			double b = ky0 - grad*kx0;
-			double agrad = fabs(grad);
-			//the y direction is the main direction in the increase of step
-			if (agrad >= 1) 
-			{
-				int y_s, y_e;
-				if (grad > 0)
-				{
-					if (ky0 <= ky1)
-					{
-						y_s = ceil((ky0 - lowboundary) / divided_length) + 1;
-						y_e = floor((ky1 - lowboundary) / divided_length) + 1;
-					}
-					else 
-					{
-						y_s = ceil((ky1 - lowboundary) / divided_length) + 1;
-						y_e = floor((ky0 - lowboundary) / divided_length) + 1;
-					}
-					double y_rs = divided_length*(y_s - 1) + lowboundary;
-					double x_rt = 1 / grad*(y_rs - b);
-					for (int j = y_s; j <= y_e; j++) 
-					{
-						int x_t = round((x_rt - leftboudary) / divided_length) + 1;
-						Line_points TEMPRO1 = { x_t ,j ,num ,theta0 };
-						linepoint.push_back(TEMPRO1);
-						p++;
-						x_rt = x_rt + 1 / grad*divided_length;
-					}
-				}
-				else if (grad < 0)
-				{
-					if (ky0 <= ky1)
-					{
-						y_s = floor((ky1 - lowboundary) / divided_length) + 1;
-						y_e = ceil((ky0 - lowboundary) / divided_length) + 1;
-					}
-					else
-					{
-						y_s = floor((ky0 - lowboundary) / divided_length) + 1;
-						y_e = ceil((ky1 - lowboundary) / divided_length) + 1;
-					}
-					double y_rs = divided_length*(y_s - 1) + lowboundary;
-					double x_rt = 1 / grad*(y_rs - b);
-					for (int j = y_s; j >= y_e; j--)
-					{
-						int x_t = round((x_rt - leftboudary) / divided_length) + 1;
-						Line_points TEMPRO1 = { x_t ,j ,num ,theta0 };
-						linepoint.push_back(TEMPRO1);
-						p++;
-						x_rt = x_rt - 1 / grad*divided_length;
-					}
-				}
-			}
-			else if (agrad < 1)
-			{
-				//the x direction is the main direction in the increase of step
-				int x_s, x_e;
-				if (grad > 0)
-				{
-					if (kx0 <= kx1)
-					{
-						x_s = ceil((kx0 - leftboudary) / divided_length) + 1;
-						x_e = floor((kx1 - leftboudary) / divided_length) + 1;
-					}
-					else 
-					{
-						x_s = ceil((kx1 - leftboudary) / divided_length) + 1;
-						x_e = floor((kx0 - leftboudary) / divided_length) + 1;
-					}
-					double x_rs = divided_length*(x_s - 1) + leftboudary;
-					double y_rt = grad*x_rs + b;
-					for (int i = x_s; i <= x_e; i++) 
-					{
-						int y_t = round((y_rt - lowboundary) / divided_length)+ 1;
-						Line_points TEMPRO1 = { i ,y_t ,num ,theta0 };
-						linepoint.push_back(TEMPRO1);
-						p++;
-						y_rt = y_rt + grad*divided_length;
-						std::cout << i << " " << y_t << " " << (p-1) << std::endl;
-					}
-				}
-				else if (grad < 0)
-				{
-					if (kx0 <= kx1)
-					{
-						x_s = floor((kx1 - leftboudary) / divided_length) + 1;
-						x_e = ceil((kx0 - leftboudary) / divided_length) + 1;
-					}
-					else 
-					{
-						x_s = floor((kx0 - leftboudary) / divided_length) + 1;
-						x_e = ceil((kx1 - leftboudary) / divided_length) + 1;
-					}
-					double x_rs = divided_length*(x_s - 1) + leftboudary;
-					double y_rt = grad*x_rs + b;
-					for (int i = x_s; i >= x_e; i--) 
-					{
-						int y_t = round((y_rt - lowboundary) / divided_length)+ 1;
-						Line_points TEMPRO1 = { i ,y_t ,num ,theta0 };
-						linepoint.push_back(TEMPRO1);
-						p++;
-						y_rt = y_rt - grad*divided_length;
-					}
-				}
-			}
-		}
-	}
-	return p;
-}
 
 
 preprocessedObservation::preprocessedObservation(
@@ -308,18 +113,195 @@ bool preprocessedObservation::getTwoMatchRate(const MatrixWrapper::ColumnVector 
 {
 	modelMatchRate = 0.0;
 	observationMatchRate = 0.0;
+	
+	int realweight = 0;//weight of the position
+  double tran_x = state(1);
+	double tran_y=  state(2);
+	double tran_theta = state(3);
+	int valid_segments = (int)available_segments.size();
 
-	std::vector <Line_points> linepoint;
-	int size =0 ;
-	int realweight = 0;
+	std::vector <Line_points> linepoint;//useful points to carculate weight
+	std::vector <SegmentStats> worked_segments;//segments in world coordinate
+	std::vector<SegmentStats> &s = available_segments;	
+	SegmentStats tempor;
 
-	/*
-	coor_trans(state);
-	size = plot_lines();
-	*/
-	//size = coor_tans_and_plot_lines(state, linepoint);
+	//transfer the points from the robotic coordinate to world coordinate
+	for (int i = 0; i < valid_segments; i++)
+	{
+		double x0_temp=s[i].x0;
+		double y0_temp=s[i].y0;
+		double x1_temp=s[i].x1;
+		double y1_temp=s[i].y1;
+		tempor.x0 = cos(tran_theta)*x0_temp - sin(tran_theta)*y0_temp + tran_x;
+	  tempor.y0 = sin(tran_theta)*x0_temp + cos(tran_theta)*y0_temp + tran_y;
+		tempor.x1 = cos(tran_theta)*x1_temp - sin(tran_theta)*y1_temp + tran_x;
+		tempor.y1 = sin(tran_theta)*x1_temp + cos(tran_theta)*y1_temp + tran_y;
+		worked_segments.push_back(tempor);
+	}
 
-	for (int num_0 = 0; num_0 < size; num_0++) {
+	//int p = 0;
+  //linepoint.clear();
+
+	//carculate the number of points in lines:
+	for (int num = 0; num < valid_segments; num++)
+	{
+		double kx0 = worked_segments[num].x0;
+		double kx1 = worked_segments[num].x1;
+		double ky0 = worked_segments[num].y0;
+		double ky1 = worked_segments[num].y1;
+		//line is vertical
+		if (kx0 == kx1)
+		{
+			int x_temp = round((kx0 - leftboudary) / divided_length) + 1;
+			int y_s, y_e;
+			if (ky0 <= ky1) 
+			{
+				y_s = ceil((ky0 - lowboundary) / divided_length) + 1;
+				y_e = floor((ky1 - lowboundary) / divided_length) + 1;
+			}
+			else
+			{
+				y_s = ceil((ky1 - lowboundary) / divided_length) + 1;
+				y_e = floor((ky0 - lowboundary) / divided_length) + 1;
+			}
+			for (int j = y_s; j <= y_e; j++) 
+			{
+				Line_points TEMPRO1 = { x_temp ,j ,num ,0 };
+				linepoint.push_back(TEMPRO1);
+			}
+		}
+		//line is horizontal
+		else if (ky0 == ky1)
+		{
+			int y_temp = round((ky0 - lowboundary) / divided_length) + 1;
+			int x_s, x_e;
+			if (kx0 <= kx1) 
+			{
+				x_s = ceil((kx0 - leftboudary) / divided_length) + 1;
+				x_e = floor((kx1 - leftboudary) / divided_length) + 1;
+			}
+			else 
+			{
+				x_s = ceil((kx1 - leftboudary) / divided_length) + 1;
+				x_e = floor((kx0 - leftboudary) / divided_length) + 1;
+			}
+			for (int j = x_s; j <= x_e; j++)
+			{
+				Line_points TEMPRO1 = { j ,y_temp ,num ,90 };
+				linepoint.push_back(TEMPRO1);
+			}
+		}
+		else 
+		{
+			double grad = (ky1 - ky0) / (kx1 - kx0);
+			double theta0 = atan(grad) * 180 / pi;
+			double b = ky0 - grad*kx0;
+			double agrad = fabs(grad);
+			//the y direction is the main direction in the increase of step
+			if (agrad >= 1) 
+			{
+				int y_s, y_e;
+				if (grad > 0)
+				{
+					if (ky0 <= ky1)
+					{
+						y_s = ceil((ky0 - lowboundary) / divided_length) + 1;
+						y_e = floor((ky1 - lowboundary) / divided_length) + 1;
+					}
+					else 
+					{
+						y_s = ceil((ky1 - lowboundary) / divided_length) + 1;
+						y_e = floor((ky0 - lowboundary) / divided_length) + 1;
+					}
+					double y_rs = divided_length*(y_s - 1) + lowboundary;
+					double x_rt = 1 / grad*(y_rs - b);
+					for (int j = y_s; j <= y_e; j++) 
+					{
+						int x_t = round((x_rt - leftboudary) / divided_length) + 1;
+						Line_points TEMPRO1 = { x_t ,j ,num ,theta0 };
+						linepoint.push_back(TEMPRO1);
+						x_rt = x_rt + 1 / grad*divided_length;
+					}
+				}
+				else if (grad < 0)
+				{
+					if (ky0 <= ky1)
+					{
+						y_s = floor((ky1 - lowboundary) / divided_length) + 1;
+						y_e = ceil((ky0 - lowboundary) / divided_length) + 1;
+					}
+					else
+					{
+						y_s = floor((ky0 - lowboundary) / divided_length) + 1;
+						y_e = ceil((ky1 - lowboundary) / divided_length) + 1;
+					}
+					double y_rs = divided_length*(y_s - 1) + lowboundary;
+					double x_rt = 1 / grad*(y_rs - b);
+					for (int j = y_s; j >= y_e; j--)
+					{
+						int x_t = round((x_rt - leftboudary) / divided_length) + 1;
+						Line_points TEMPRO1 = { x_t ,j ,num ,theta0 };
+						linepoint.push_back(TEMPRO1);
+						x_rt = x_rt - 1 / grad*divided_length;
+					}
+				}
+			}
+			else if (agrad < 1)
+			{
+				//the x direction is the main direction in the increase of step
+				int x_s, x_e;
+				if (grad > 0)
+				{
+					if (kx0 <= kx1)
+					{
+						x_s = ceil((kx0 - leftboudary) / divided_length) + 1;
+						x_e = floor((kx1 - leftboudary) / divided_length) + 1;
+					}
+					else 
+					{
+						x_s = ceil((kx1 - leftboudary) / divided_length) + 1;
+						x_e = floor((kx0 - leftboudary) / divided_length) + 1;
+					}
+					double x_rs = divided_length*(x_s - 1) + leftboudary;
+					double y_rt = grad*x_rs + b;
+					for (int i = x_s; i <= x_e; i++) 
+					{
+						int y_t = round((y_rt - lowboundary) / divided_length)+ 1;
+						Line_points TEMPRO1 = { i ,y_t ,num ,theta0 };
+						linepoint.push_back(TEMPRO1);
+						y_rt = y_rt + grad*divided_length;
+						std::cout << i << " " << y_t << " " << (p-1) << std::endl;
+					}
+				}
+				else if (grad < 0)
+				{
+					if (kx0 <= kx1)
+					{
+						x_s = floor((kx1 - leftboudary) / divided_length) + 1;
+						x_e = ceil((kx0 - leftboudary) / divided_length) + 1;
+					}
+					else 
+					{
+						x_s = floor((kx0 - leftboudary) / divided_length) + 1;
+						x_e = ceil((kx1 - leftboudary) / divided_length) + 1;
+					}
+					double x_rs = divided_length*(x_s - 1) + leftboudary;
+					double y_rt = grad*x_rs + b;
+					for (int i = x_s; i >= x_e; i--) 
+					{
+						int y_t = round((y_rt - lowboundary) / divided_length)+ 1;
+						Line_points TEMPRO1 = { i ,y_t ,num ,theta0 };
+						linepoint.push_back(TEMPRO1);
+						y_rt = y_rt - grad*divided_length;
+					}
+				}
+			}
+		}
+	}
+
+	int point_size=(int)linepoint.size():
+
+	for (int num_0 = 0; num_0 < point_size; num_0++) {
 		double theta1= linepoint[num_0].theta / 10;
 		int theta_temp = round(theta1) + 9;
 		int x_temp = round(linepoint[num_0].x);
